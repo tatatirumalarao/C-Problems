a7ec59f8bda63769fdea325c38df04c254cbd554
tatatirumalarao <tiruyekambaram@gmail.com>
2026-01-15 17:52:06 +0000

    Add files via upload

---
+++ 15.01.2026/c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    int i,n,sum=0;
+    for(i=0;i<n;i++)
+    {
+        sum=sum+i;
+    }
+    return 0;
+}
---
+++ 15.01.2026/cc.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+ {
+    printf("tiru");
+    return 0;
+}
