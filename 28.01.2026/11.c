4d0619e410096e19b68b9a1885f0f6642b336d51
tatatirumalarao <tiruyekambaram@gmail.com>
2026-01-28 17:05:48 +0000

    Add files via upload

---
+++ 28.01.2026/11.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, len;
+    char temp;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    len = strlen(str);
+    if (str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
+    for (i = 0; i < len / 2; i++) {
+        temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
