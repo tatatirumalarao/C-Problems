eb7d46a5e047f13901f83106b38e69e001b40ca8
tatatirumalarao <tiruyekambaram@gmail.com>
2026-02-05 17:02:43 +0000

    Add files via upload

---
+++ 05.02.2026/cc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int add(int a, int b) {
+    return a + b;
+}
+int subtract(int a, int b) {
+    return a - b;
+}
+int multiply(int a, int b) {
+    return a * b;
+}
+int divide(int a, int b) {
+    return a / b;  
+}
+int main() {
+    printf("10 + 5 = %d\n", add(10, 5));
+    printf("10 - 5 = %d\n", subtract(10, 5));
+    printf("10 * 5 = %d\n", multiply(10, 5));
+    printf("10 / 5 = %d\n", divide(10, 5));
+    printf("20 / 4 = %d\n", divide(20, 4));
+    return 0;
+}
