4b64e8e97c05dc2ccc7acbe3dc175943a06e4e14
tatatirumalarao <tiruyekambaram@gmail.com>
2026-02-22 07:06:34 +0000

    Add files via upload

---
+++ 22.02.2026/5.0.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a=9,b=2;
+    int c=a%=b;
+    printf("%d",c);
+    //Your Code goes here!
+    
+    return 0;
+}
---
+++ 22.02.2026/5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    scanf("%d%d",&a,&b);
+     
+
+
+    if(a>0  || b>0)
+    {
+        printf("1\n");
+    }
+    else 
+    {
+        printf("0\n");
+    }
+    //Your Code goes here!
+    
+    return 0;
+}
