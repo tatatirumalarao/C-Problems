14cb250852c53567a84c2adfc53e0db7ab67764c
tatatirumalarao <tiruyekambaram@gmail.com>
2026-02-24 13:53:31 +0000

    Add files via upload

---
+++ 24.02.2026/7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a=20,b=10;
+    if(a>b && a<b)
+    {
+        printf("a is greater");
+    }
+    else 
+    {
+        printf("b is greater");
+    }
+    //Your Code goes here!
+    
+    return 0;
+}
