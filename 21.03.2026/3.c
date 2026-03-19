f2765e3cccdb613f7144240651f94321cc00cee7
tatatirumalarao <tiruyekambaram@gmail.com>
2026-03-20 00:09:47 +0530

    Add files via upload

---
+++ 21.03.2026/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i,n;
+    scanf("%d",&n);
+    for(i=2;i<=n;i=i+2)
+    {
+        printf("%d",i);
+    }
+    //Your Code goes here!
+    
+    return 0;
+}
