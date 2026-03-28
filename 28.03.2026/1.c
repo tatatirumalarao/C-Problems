f137b0e03d5a2eb88f9cb4fa0e1ef4a82726ca50
tatatirumalarao <tiruyekambaram@gmail.com>
2026-03-28 21:05:23 +0530

    Add files via upload

---
+++ 28.03.2026/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i);
+    }
+    //Your Code goes here!
+    
+    return 0;
+}
