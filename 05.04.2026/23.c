8757c2419171d298e42ad3a0aaedc39f299aa943
tatatirumalarao <tiruyekambaram@gmail.com>
2026-04-05 17:37:01 +0530

    Add files via upload

---
+++ 05.04.2026/23.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("positive");
+    }
+    else
+    {
+        printf("negative");
+    }
+    //Your Code goes here!
+    
+    return 0;
+}
