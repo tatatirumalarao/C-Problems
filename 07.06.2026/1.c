de25ace5ff6100e6886fe46341f9763fbe23495c
tatatirumalarao <tiruyekambaram@gmail.com>
2026-06-07 20:37:15 +0530

    Add files via upload

---
+++ 07.06.2026/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    int num;
+    printf("enter the number : ");
+    scanf("%d",&num);
+    num=num+2;
+    printf("after value is %d",num);   
+    return 0;
+}
