6aab753cb6b1f6a28aa6cead50ff3195cbaf4588
tatatirumalarao <tiruyekambaram@gmail.com>
2026-08-01 00:02:42 +0530

    Add files via upload

---
+++ 01.08.2026/2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() 
+{
+    int A=20;
+    int B=50;
+    int C=A+B;
+    printf("The value of C is = %0x",C);  // C is the name of the varible //    d is called decimal number
+}
---
+++ 01.08.2026/3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() 
+{
+    char A=65;
+    char B=65;
+    char C=A+B;
+    printf("The value of C is %d",C);
+}
